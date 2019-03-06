c1fbc70fefa08e542d9f51c9227b2fcfb2bd9e4d
ViotSebastien <viot.sebastien@oostaoo.com>
2019-03-06 14:33:51 +0100

    update menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,7 @@ const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
-$('.dropdown-toggle').dropdown()
+
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
